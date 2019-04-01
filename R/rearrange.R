8045f14a076851c2cc6ddb8418a1dbb6573bd61d
James Dole <jimdole87@gmail.com>
2019-04-01 15:03:30 -0500

    Added number of elements per question output for rearrange function

--- R/rearrange.R
+++ R/rearrange.R
@@ -1,8 +1,14 @@
 x = list(c(1, "a", "b"), c(2, "a", "b", "c"), c(3, "a", "b", "c", "d", "e"))
 
 rearrange = function(x){
-  #put checks here
+
+
   num_ques = length(x)
+  QA = rep(NA, num_ques)
+  for(i in 1:num_ques){
+    QA[i] = length(x[[i]]) # number of answers +1 per question
+  }
+
   ind = sample(num_ques)
   reorder_ans = x
   for(i in 1:num_ques){
@@ -10,20 +16,26 @@ rearrange = function(x){
     ind_ans = sample(2:num_ans)
     for(j in 2:num_ans){
       reorder_ans[[i]][j] = x[[i]][ind_ans[j - 1]]
+
     }
   }
   reorder_x = list(NA)
+  reorder_QA = rep(NA, num_ques)
   for(i in 1:num_ques){
     reorder_x[i] = reorder_ans[ind[i]]
+    reorder_QA[i] = QA[ind[i]]
   }
-  return(reorder_x)
+  #Output string of question and answer
+  #Output number of elements per question(eg 1 question with 3 answers outputs 4)
+  return(list(reorder_x, reorder_QA))
 }
 
 xre = rearrange(x)
 
-xre_unlist = unlist(xre)
+xre_unlist = unlist(xre[1])
 xre_unlist
 
+xre[2]
 recreation = function(x){
   #totalout = "header stuff and question 1" FIX THIS
   for(i in 2:length(x)){
