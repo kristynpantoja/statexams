af8f5b34c7479ebe6b2d424a1566b987389d250e
Kristyn Pantoja <kristynp@tamu.edu>
2019-04-10 14:30:37 -0500

    added Normal question-making function

--- R/kristynFunctions.R
+++ R/kristynFunctions.R
@@ -1,6 +1,5 @@
 
-# --- Functions --- #
-
+# --- Functions for Importing/Exporting to/from a .txt File --- #
 
 import_question = function(file){
   # 1. read in the file. it has 2 parts: the name of the file, and the text inside the file.
@@ -34,4 +33,55 @@ export_txt = function(contents, filename){
   close(file_connection)
   return("exported txt file")
 }
+
+
+# --- Functions for Creating Questions for Normal Distribution Calculations --- #
+
+makeQuestion_normal = function(variable = "X", mean = 0, sd = 1, interval, tail = NULL){
+  # Cleaning up arguments and error-catching
+  if(length(interval) == 1 & is.null(tail)) stop("Need to specify type of tail probability")
+  if(!is.null(tail)){
+    if(tail == "l" | tail == "left") tail = 0
+    else if(tail == "r" | tail == "right") tail = 1
+    else stop("Need to specify a valid type of tail probability: left or right")
+  }
+  if(is.null(tail)) tail = 2
+
+  base_component1 = " is normally distributed with mean "
+  base_component2 = " and standard deviation "
+  base_component3 = ". What is the probability that "
+  context_component1 = paste(variable, base_component1, mean, base_component2, sd, base_component3, sep = "")
+
+  context_component2 = ""
+  # Question for Left-tail probabilities
+  if(length(interval) == 1 & tail == 0){
+    base_component4 = " is less than "
+    context_component2 = paste(variable, base_component4, interval, "?", sep = "")
+  }
+
+  # Question for Right-tail probabilities
+  if(length(interval) == 1 & tail == 1){
+    base_component4 = " is greater than "
+    context_component2 = paste(variable, base_component4, interval, "?", sep = "")
+  }
+
+  # Question for interval probabilities
+  if(length(interval) == 2){
+    base_component4 = " is between "
+    base_component5 = " and "
+    context_component2 = paste(variable, base_component4, interval[1],
+                               base_component5, interval[2], "?", sep = "")
+  }
+
+  question = paste(context_component1, context_component2, sep = "")
+  return(question)
+}
+
+# add makeAnswers_normal
+
+# add generate_normal
+
+
+
+
 
