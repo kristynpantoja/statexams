41e46f2c3a3c7acf6b32bf5a70890af1a3dee2ca
James Dole <jimdole87@gmail.com>
2019-04-28 23:24:08 -0500

    added test functions

--- R/testoutput.R
+++ R/testoutput.R
@@ -2,6 +2,19 @@
 
 library(readtext)
 source("R/kristynFunctions.R")
+source("R/jamesFunctions.R")
+source("R/rearrange.R")
+
+testquestion1 = makeQA_eventprob()
+testquestion2 = makeQA_ExpectedValue()
+testquestion3 = makeQA_HypothesisTest()
+
+test = list(testquestion1, testquestion2, testquestion3)
+testrearrange = rearrange(test)
+testrearrange
+
 
 question_contents = import_question("samplequestionlabeledparts.txt")
 process_question_contents = process_question_contents(question_contents)
+rearrange(process_question_contents)
+length(process_question_contents)
